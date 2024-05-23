8306f37bde0cc37bb637cc6c86bd501c6c41c7ed
daier <ys2017rhein@163.com>
2024-05-23 19:30:14 +0800

    111

--- src/main/java/com/rhein/bolgs/blogsweb/Application.java
+++ src/main/java/com/rhein/bolgs/blogsweb/Application.java
@@ -1,13 +1,16 @@
 package com.rhein.bolgs.blogsweb;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogswebApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogswebApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
